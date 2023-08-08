76efbd17c82e452bc5f81550335e88f680dc0828
sharleenawinja <awinjasharleen@gmail.com>
2023-08-08 15:19:46 +0300

    starting on navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 
+import Navbar from "@/app/components/navbar/Navbar";
+
 const nunito = Nunito({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,6 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Navbar />
       <body className={nunito.className}>{children}</body>
     </html>
   );
